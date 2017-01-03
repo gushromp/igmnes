f5e3acb063dc64036aeffa357773d78c83b30190
Ivan Đorđević <i.djordjevic@itsinbox.com>
2017-01-03 13:21:49 +0100

    Decode more instructions

--- src/core/instructions.rs
+++ src/core/instructions.rs
@@ -177,7 +177,7 @@ impl Instruction {
             //
             // ALU instructions
             //
-            // ORA bitwise OR with Accumulator
+            // ORA (bitwise OR with Accumulator)
             0x09 => Instruction::new(ORA, Immediate, 2, 2),
             0x05 => Instruction::new(ORA, ZeroPage, 2, 3),
             0x15 => Instruction::new(ORA, ZeroPageIndexedX, 2, 4),
@@ -186,6 +186,104 @@ impl Instruction {
             0x19 => Instruction::new(ORA, AbsoluteIndexedY, 3, 4),
             0x01 => Instruction::new(ORA, IndexedIndirectX, 2, 6),
             0x11 => Instruction::new(ORA, IndirectIndexedY, 2, 5),
+            // AND (bitwise AND with accumulator)
+            0x29 => Instruction::new(AND, Immediate, 2, 2),
+            0x25 => Instruction::new(AND, ZeroPage, 2, 3),
+            0x35 => Instruction::new(AND, ZeroPageIndexedX, 2, 4),
+            0x2D => Instruction::new(AND, Absolute, 3, 4),
+            0x3D => Instruction::new(AND, AbsoluteIndexedX, 3, 4),
+            0x39 => Instruction::new(AND, AbsoluteIndexedY, 3, 4),
+            0x21 => Instruction::new(AND, IndexedIndirectX, 2, 6),
+            0x31 => Instruction::new(AND, IndirectIndexedY, 2, 5),
+            // EOR (bitwise Exclusive OR)
+            0x49 => Instruction::new(EOR, Immediate, 2, 2),
+            0x45 => Instruction::new(EOR, ZeroPage, 2, 3),
+            0x55 => Instruction::new(EOR, ZeroPageIndexedX, 2, 4),
+            0x4D => Instruction::new(EOR, Absolute, 3, 4),
+            0x5D => Instruction::new(EOR, AbsoluteIndexedX, 3, 4),
+            0x59 => Instruction::new(EOR, AbsoluteIndexedY, 3, 4),
+            0x41 => Instruction::new(EOR, IndexedIndirectX, 2, 6),
+            0x51 => Instruction::new(EOR, IndirectIndexedY, 2, 5),
+            // ADC (ADd with Carry)
+            0x69 => Instruction::new(ADC, Immediate, 2, 2),
+            0x65 => Instruction::new(ADC, ZeroPage, 2, 3),
+            0x75 => Instruction::new(ADC, ZeroPageIndexedX, 2, 4),
+            0x6D => Instruction::new(ADC, Absolute, 3, 4),
+            0x7D => Instruction::new(ADC, AbsoluteIndexedX, 3, 4),
+            0x79 => Instruction::new(ADC, AbsoluteIndexedY, 3, 4),
+            0x61 => Instruction::new(ADC, IndexedIndirectX, 2, 6),
+            0x71 => Instruction::new(ADC, IndirectIndexedY, 2, 5),
+            // CMP (CoMPare accumulator)
+            0xC9 => Instruction::new(CMP, Immediate, 2, 2),
+            0xC5 => Instruction::new(CMP, ZeroPage, 2, 3),
+            0xD5 => Instruction::new(CMP, ZeroPageIndexedX, 2, 4),
+            0xCD => Instruction::new(CMP, Absolute, 3, 4),
+            0xDD => Instruction::new(CMP, AbsoluteIndexedX, 3, 4),
+            0xD9 => Instruction::new(CMP, AbsoluteIndexedY, 3, 4),
+            0xC1 => Instruction::new(CMP, IndexedIndirectX, 2, 6),
+            0xD1 => Instruction::new(CMP, IndirectIndexedY, 2, 5),
+            //
+            // Read/Modify/Write instructions
+            //
+            // ASL (Arithmetic Shift Left)
+            0x0A => Instruction::new(ASL, Accumulator, 1, 2),
+            0x06 => Instruction::new(ASL, ZeroPage, 2, 5),
+            0x16 => Instruction::new(ASL, ZeroPageIndexedX, 2, 6),
+            0x0E => Instruction::new(ASL, Absolute, 3, 6),
+            0x1E => Instruction::new(ASL, AbsoluteIndexedX, 3, 7),
+            // ROL (ROtate Left)
+            0x2A => Instruction::new(ROL, Accumulator, 1, 2),
+            0x26 => Instruction::new(ROL, ZeroPage, 2, 5),
+            0x36 => Instruction::new(ROL, ZeroPageIndexedX, 2, 6),
+            0x2E => Instruction::new(ROL, Absolute, 3, 6),
+            0x3E => Instruction::new(ROL, AbsoluteIndexedX, 3, 7),
+            // ASL (Arithmetic Shift Left)
+            0x0A => Instruction::new(ASL, Accumulator, 1, 2),
+            0x06 => Instruction::new(ASL, ZeroPage, 2, 5),
+            0x16 => Instruction::new(ASL, ZeroPageIndexedX, 2, 6),
+            0x0E => Instruction::new(ASL, Absolute, 3, 6),
+            0x1E => Instruction::new(ASL, AbsoluteIndexedX, 3, 7),
+            //
+            // Store/Load instructions
+            //
+            // LDA (LoaD Accumulator)
+            0xA9 => Instruction::new(LDA, Immediate, 2, 2),
+            0xA5 => Instruction::new(LDA, ZeroPage, 2, 3),
+            0xB5 => Instruction::new(LDA, ZeroPageIndexedX, 2, 4),
+            0xAD => Instruction::new(LDA, Absolute, 3, 4),
+            0xBD => Instruction::new(LDA, AbsoluteIndexedX, 3, 4),
+            0xB9 => Instruction::new(LDA, AbsoluteIndexedY, 3, 4),
+            0xA1 => Instruction::new(LDA, IndexedIndirectX, 2, 6),
+            0xB1 => Instruction::new(LDA, IndirectIndexedY, 2, 5),
+            // LDX (LoaD X register)
+            0xA2 => Instruction::new(LDX, Immediate, 2, 2),
+            0xA6 => Instruction::new(LDX, ZeroPage, 2, 3),
+            0xB6 => Instruction::new(LDX, ZeroPageIndexedY, 2, 4),
+            0xAE => Instruction::new(LDX, Absolute, 3, 4),
+            0xBE => Instruction::new(LDX, AbsoluteIndexedY, 3, 4),
+            // LDY (LoaD Y register)
+            0xA0 => Instruction::new(LDY, Immediate, 2, 2),
+            0xA4 => Instruction::new(LDY, ZeroPage, 2, 3),
+            0xB4 => Instruction::new(LDY, ZeroPageIndexedX, 2, 4),
+            0xAC => Instruction::new(LDY, Absolute, 3, 4),
+            0xBC => Instruction::new(LDY, AbsoluteIndexedX, 3, 4),
+            // STA (STore Accumulator)
+            0x85 => Instruction::new(STA, ZeroPage, 2, 3),
+            0x95 => Instruction::new(STA, ZeroPageIndexedX, 2, 4),
+            0x8D => Instruction::new(STA, Absolute, 3, 4),
+            0x9D => Instruction::new(STA, AbsoluteIndexedX, 3, 5),
+            0x99 => Instruction::new(STA, AbsoluteIndexedY, 3, 5),
+            0x81 => Instruction::new(STA, IndexedIndirectX, 2, 6),
+            0x91 => Instruction::new(STA, IndirectIndexedY, 2, 6),
+            // STX (STore X register)
+            0x86 => Instruction::new(STX, ZeroPage, 2, 3),
+            0x96 => Instruction::new(STX, ZeroPageIndexedY, 2, 4),
+            0x8E => Instruction::new(STX, Absolute, 3, 4),
+            // STY (STore Y register)
+            0x84 => Instruction::new(STY, ZeroPage, 2, 3),
+            0x94 => Instruction::new(STY, ZeroPageIndexedX, 2, 4),
+            0x8C => Instruction::new(STY, Absolute, 3, 4),
+
         }
     }
 }
