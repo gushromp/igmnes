b1d7f5bf9aef24a1ab069ea506b53e7fd1ac2f9c
Ivan Đorđević <i.djordjevic@itsinbox.com>
2017-01-14 19:50:25 +0100

    Parse APU registers

--- src/core/apu.rs
+++ src/core/apu.rs
@@ -2,6 +2,7 @@
 use core::memory::MemMapped;
 use core::errors::EmulationError;
 
+// Length counter lookup table
 const LC_LOOKUP_TABLE: [u8; 32] =
 [10, 254, 20, 2, 40, 4, 80, 6,
     160, 8, 60, 10, 14, 12, 26, 14,
@@ -24,15 +25,66 @@ struct Pulse {
     // Length counter load (L), timer high (T)
     reg_lllllttt: u8,
 
+    // Duty for current APU frame
+    pub duty: u8,
+    // Volume for current APU frame
+    pub constant_volume: bool,
+    pub volume: u8,
+    // Sweep:
+    pub sweep_enabled: bool,
+    pub sweep_period: u8,
+    pub sweep_negate: bool,
+    pub sweep_shift: u8,
     // Timer is 11-bit (bits 11-15 are disregarded)
-    timer: u16,
+    pub timer: u16,
     // Length counter is 5-bit (bits 5-7 are disregarded)
-    length_counter: u8,
+    pub lc_halt_env_loop: bool,
+    pub length_counter: u8,
+}
+
+impl Pulse {
+    fn write_ddlcvvvv(&mut self, byte: u8) {
+        self.duty = byte >> 6;
+        self.constant_volume = byte & 0b0001_0000 != 0;
+        self.volume = byte & 0b1111;
+        self.lc_halt_env_loop = byte & 0b0010_0000 != 0;
+
+        self.reg_ddlcvvvv = byte;
+    }
+
+    fn write_epppnsss(&mut self, byte: u8) {
+        self.sweep_enabled = byte & 0b1000_0000 != 0;
+        self.sweep_period = (byte >> 4) & 0b111;
+        self.sweep_negate = byte & 0b0000_1000 != 0;
+        self.sweep_shift = byte & 0b111;
+
+        self.reg_epppnsss;
+    }
+
+    fn write_tttttttt(&mut self, byte: u8) {
+        let timer_high = self.timer >> 8;
+        let timer_low = byte as u16;
+        self.timer = (timer_high << 8) | timer_low;
+
+        self.reg_tttttttt = byte;
+    }
+
+    fn write_lllllttt(&mut self, byte: u8) {
+        let timer_high = (byte & 0b111) as u16;
+        self.timer = (timer_high << 8) | self.reg_tttttttt as u16;
+
+        let length_counter_index = (byte >> 3) as usize;
+        self.length_counter = LC_LOOKUP_TABLE[length_counter_index];
+
+        self.reg_lllllttt = byte;
+    }
+
 }
 
 impl ApuChannel for Pulse {
-    fn step(&mut self) {
 
+    fn step(&mut self) {
+        self.length_counter -= 1;
     }
 
     fn output(&self) -> u8 {
@@ -50,6 +102,43 @@ struct Triangle {
     reg_tttttttt: u8,
     // Length counter load (L), timer high (T)
     reg_lllllttt: u8,
+
+    pub lengthc_halt_linearc_control: bool,
+    pub linear_counter_load: u8,
+
+    pub timer: u16,
+    pub length_counter: u8,
+}
+
+impl Triangle {
+    fn write_crrrrrrr(&mut self, byte: u8) {
+        self.lengthc_halt_linearc_control = byte & 0b1000_0000 != 0;
+        self.linear_counter_load = byte & 0b0111_1111;
+
+        self.reg_crrrrrrr = byte;
+    }
+
+    fn write_uuuuuuuu(&mut self, byte: u8) {
+        self.reg_uuuuuuuu = byte;
+    }
+
+    fn write_tttttttt(&mut self, byte: u8) {
+        let timer_high = self.timer >> 8;
+        let timer_low = byte as u16;
+        self.timer = (timer_high << 8) | timer_low;
+
+        self.reg_tttttttt = byte;
+    }
+
+    fn write_lllllttt(&mut self, byte: u8) {
+        let timer_high = byte & 0b111;
+        self.timer = ((timer_high as u16) << 8) | self.reg_tttttttt as u16;
+
+        let length_counter_index = (byte >> 3) as usize;
+        self.length_counter = LC_LOOKUP_TABLE[length_counter_index];
+
+        self.reg_lllllttt = byte;
+    }
 }
 
 impl ApuChannel for Triangle {
@@ -72,6 +161,42 @@ struct Noise {
     reg_luuupppp: u8,
     // Length counter load (L), unused (U)
     reg_llllluuu: u8,
+
+    pub volume: u8,
+    pub lc_halt_env_loop: bool,
+    pub constant_volume: bool,
+    pub noise_loop: bool,
+    pub noise_period: u8,
+
+    pub length_counter: u8,
+}
+
+impl Noise {
+    fn write_uulcvvvv(&mut self, byte: u8) {
+        self.lc_halt_env_loop = byte & 0b0010_0000 != 0;
+        self.constant_volume = byte & 0b0001_0000 != 0;
+        self.volume = byte & 0b1111;
+
+        self.reg_uulcvvvv = byte;
+    }
+
+    fn write_uuuuuuuu(&mut self, byte: u8) {
+        self.reg_uuuuuuuu = byte;
+    }
+
+    fn write_luuupppp(&mut self, byte: u8) {
+        self.noise_loop = byte & 0b1000_0000 != 0;
+        self.noise_period = byte & 0b1111;
+
+        self.reg_luuupppp = byte;
+    }
+
+    fn write_llllluuu(&mut self, byte: u8) {
+        let length_counter_index = (byte >> 3) as usize;
+        self.length_counter = LC_LOOKUP_TABLE[length_counter_index];
+
+        self.reg_llllluuu = byte;
+    }
 }
 
 impl ApuChannel for Noise {
@@ -94,6 +219,41 @@ struct DMC {
     reg_aaaaaaaa: u8,
     // Sample length (L)
     reg_llllllll: u8,
+
+    pub irq_enable: bool,
+    pub dmc_loop: bool,
+    pub frequency: u8,
+    pub load_counter: u8,
+    pub sample_address: u8,
+    pub sample_length: u8,
+}
+
+impl DMC {
+    fn write_iluurrrr(&mut self, byte: u8) {
+        self.irq_enable = byte & 0b1000_0000 != 0;
+        self.dmc_loop = byte & 0b0100_0000 != 0;
+        self.frequency = byte & 0b1111;
+
+        self.reg_iluurrrr = byte;
+    }
+
+    fn write_udddddddd(&mut self, byte: u8) {
+        self.load_counter = byte & 0b0111_1111;
+
+        self.reg_uddddddd = byte;
+    }
+
+    fn write_aaaaaaaa(&mut self, byte: u8) {
+        self.sample_address = byte;
+
+        self.reg_aaaaaaaa = byte;
+    }
+
+    fn write_llllllll(&mut self, byte: u8) {
+        self.sample_length = byte;
+
+        self.reg_llllllll = byte;
+    }
 }
 
 impl ApuChannel for DMC {
@@ -242,30 +402,30 @@ impl MemMapped for Apu {
         
     fn write(&mut self, addr: u16, byte: u8) -> Result<(), EmulationError> {
         match addr {
-            0x4000 => { self.pulse1.reg_ddlcvvvv = byte; Ok(()) },
-            0x4001 => { self.pulse1.reg_epppnsss = byte; Ok(()) },
-            0x4002 => { self.pulse1.reg_tttttttt = byte; Ok(()) },
-            0x4003 => { self.pulse1.reg_lllllttt = byte; Ok(()) },
+            0x4000 => { self.pulse1.write_ddlcvvvv(byte); Ok(()) },
+            0x4001 => { self.pulse1.write_epppnsss(byte); Ok(()) },
+            0x4002 => { self.pulse1.write_tttttttt(byte); Ok(()) },
+            0x4003 => { self.pulse1.write_lllllttt(byte); Ok(()) },
             // Pulse2
-            0x4004 => { self.pulse2.reg_ddlcvvvv = byte; Ok(()) },
-            0x4005 => { self.pulse2.reg_epppnsss = byte; Ok(()) },
-            0x4006 => { self.pulse2.reg_tttttttt = byte; Ok(()) },
-            0x4007 => { self.pulse2.reg_lllllttt = byte; Ok(()) },
+            0x4004 => { self.pulse2.write_ddlcvvvv(byte); Ok(()) },
+            0x4005 => { self.pulse2.write_epppnsss(byte); Ok(()) },
+            0x4006 => { self.pulse2.write_tttttttt(byte); Ok(()) },
+            0x4007 => { self.pulse2.write_lllllttt(byte); Ok(()) },
             // Triangle
-            0x4008 => { self.triangle.reg_crrrrrrr = byte; Ok(()) },
-            0x4009 => { self.triangle.reg_uuuuuuuu = byte; Ok(()) },
-            0x400A => { self.triangle.reg_tttttttt = byte; Ok(()) },
-            0x400B => { self.triangle.reg_lllllttt = byte; Ok(()) },
+            0x4008 => { self.triangle.write_crrrrrrr(byte); Ok(()) },
+            0x4009 => { self.triangle.write_uuuuuuuu(byte); Ok(()) },
+            0x400A => { self.triangle.write_tttttttt(byte); Ok(()) },
+            0x400B => { self.triangle.write_lllllttt(byte); Ok(()) },
             // Noise
-            0x400C => { self.noise.reg_uulcvvvv = byte; Ok(()) },
-            0x400D => { self.noise.reg_uuuuuuuu = byte; Ok(()) },
-            0x400E => { self.noise.reg_luuupppp = byte; Ok(()) },
-            0x400F => { self.noise.reg_llllluuu = byte; Ok(()) },
+            0x400C => { self.noise.write_uulcvvvv(byte); Ok(()) },
+            0x400D => { self.noise.write_uuuuuuuu(byte); Ok(()) },
+            0x400E => { self.noise.write_luuupppp(byte); Ok(()) },
+            0x400F => { self.noise.write_llllluuu(byte); Ok(()) },
             // DMC
-            0x4010 => { self.dmc.reg_iluurrrr = byte; Ok(()) },
-            0x4011 => { self.dmc.reg_uddddddd = byte; Ok(()) },
-            0x4012 => { self.dmc.reg_aaaaaaaa = byte; Ok(()) },
-            0x4013 => { self.dmc.reg_llllllll = byte; Ok(()) },
+            0x4010 => { self.dmc.write_iluurrrr(byte); Ok(()) },
+            0x4011 => { self.dmc.write_udddddddd(byte); Ok(()) },
+            0x4012 => { self.dmc.write_aaaaaaaa(byte); Ok(()) },
+            0x4013 => { self.dmc.write_llllllll(byte); Ok(()) },
             //
             // 0x4014 is skipped, it's not part of the APU,
             // but rather the OMA DMA register
